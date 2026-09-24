43d256b91fdae14db45966534fa250088bc21753
John Muñoz <sebastian.mh@outlook.com>
2026-09-24 18:41:41 -0500

    Se agregaron comentarios y listo para Vercel

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+//DEFINE LA ESTRUCTURA DE LA TABLA USERS EN LA BASE DE DATOS
+
 return new class extends Migration
 {
     /**
